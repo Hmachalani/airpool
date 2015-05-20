11544a7c49d2217c128aba9fc232cb353073c025
Henri Machalani <hmachalani@gmail.com>
2015-05-19 23:19:53 -0700

    Initial Commit

---
+++ airpool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  airpool
+//
+//  Created by Henri Machalani on 5/19/15.
+//  Copyright (c) 2015 airpool inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ airpool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  airpool
+//
+//  Created by Henri Machalani on 5/19/15.
+//  Copyright (c) 2015 airpool inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
